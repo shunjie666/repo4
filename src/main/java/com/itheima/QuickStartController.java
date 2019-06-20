e5a621cec47ce12360ee5a7a1c6be06e41bd81b9
zhangsan <zhangsan@itcast.cn>
2019-06-20 22:30:22 +0800

    sdxsc

---
+++ src/main/java/com/itheima/QuickStartController.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuickStartController {
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+        return "springboot 访问成功";
+    }
+}
